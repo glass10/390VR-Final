a9f57ff2acc0f05c49293de47e329cc485e9c881
avincent6 <vincent6@purdue.edu>
2019-04-18 11:10:06 -0400

    Initial commit from Unit Github Plugin - with navmesh, navmesh links, navmesh ghost agents, ghosts that follow player

---
+++ Assets/Ghost.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Ghost : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // end game
+        }
+    }
+}
+
