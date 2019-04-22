3f3b5b954ba2608034ad662e91799821da39cae7
Cameron Glass <glass10@purdue.edu>
2019-04-21 23:45:08 -0400

    Galaga Start/Pause/Stop, writing scores to board

--- Assets/Scenes/galagaMoveShip.cs
+++ Assets/Scenes/galagaMoveShip.cs
@@ -7,12 +7,18 @@ public class galagaMoveShip : MonoBehaviour
     GameObject ship;
     private RaycastHit hit;
     public GameObject m_shotPrefab;
-    public int score;
+
+    // Shared Globals 
+    public static int score = 0;
+    public static int lives = 3;
+    public static bool gameActive = false;
+    public static double gameSpeed = 1.0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Temp
+        gameActive = true;
     }
 
     // Update is called once per frame
@@ -21,97 +27,129 @@ public class galagaMoveShip : MonoBehaviour
         ship = GameObject.Find("MainShip");
 
         // Move ship and camera right
-        if (Input.GetKey(KeyCode.Period) && ship.transform.position.x < 300)
+        if (Input.GetKey(KeyCode.Period) && ship.transform.position.x < 300 && gameActive)
         {
-            //Move Camera/Player
-            Vector3 position = this.transform.position;
-            position.x += 2;
-            this.transform.position = position;
-
-            //Move Ship
-            position = ship.transform.position;
-            position.x += 2;
-            ship.transform.position = position;
+            moveRight();
         }
         // Move ship and camera left
-        if (Input.GetKey(KeyCode.Comma) && ship.transform.position.x > -300)
+        if (Input.GetKey(KeyCode.Comma) && ship.transform.position.x > -300 && gameActive)
         {
-            //Move Camera/Player
-            Vector3 position = this.transform.position;
-            position.x -= 2;
-            this.transform.position = position;
-
-            //Move Ship
-            position = ship.transform.position;
-            position.x -= 2;
-            ship.transform.position = position;
+            moveLeft();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Shoot
+        if (Input.GetKeyDown(KeyCode.Space) && gameActive)
         {
-            Debug.Log("Shoot");
-            //Create Laser
-            GameObject go = GameObject.Instantiate(m_shotPrefab, ship.transform.position, ship.transform.rotation) as GameObject;
-            GameObject.Destroy(go, 3f);
+            shoot();
+        }
 
-            //Do Raycast Things
-            Vector3 fwd = ship.transform.TransformDirection(Vector3.forward);
-            Debug.DrawRay(ship.transform.position, fwd * 500, Color.green);
-            if (Physics.Raycast(ship.transform.position, fwd * 500, out hit, 200))
+        //Temp -> Change to collision 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            loseLife();
+        }
+
+    }
+
+    void loseLife()
+    {
+        uiScript.pauseGame(true);
+    }
+
+    void moveLeft()
+    {
+        //Move Camera/Player
+        Vector3 position = this.transform.position;
+        position.x -= 2;
+        this.transform.position = position;
+
+        //Move Ship
+        position = ship.transform.position;
+        position.x -= 2;
+        ship.transform.position = position;
+    }
+
+    void moveRight()
+    {
+        //Move Camera/Player
+        Vector3 position = this.transform.position;
+        position.x += 2;
+        this.transform.position = position;
+
+        //Move Ship
+        position = ship.transform.position;
+        position.x += 2;
+        ship.transform.position = position;
+    }
+
+    void shoot()
+    {
+        Debug.Log("Shoot");
+        //Create Laser
+        GameObject go = GameObject.Instantiate(m_shotPrefab, ship.transform.position, ship.transform.rotation) as GameObject;
+        GameObject.Destroy(go, 3f);
+
+        //Do Raycast Things
+        Vector3 fwd = ship.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(ship.transform.position, fwd * 500, Color.green);
+        if (Physics.Raycast(ship.transform.position, fwd * 500, out hit, 200))
+        {
+            Debug.Log("Hit " + hit.transform.name);
+            if (hit.transform.name != "Backdrop Stars")
             {
-                Debug.Log("Hit " + hit.transform.name);
-                if(hit.transform.name != "Backdrop Stars")
+                // Get gameobj from ship hit
+                GameObject obj = GameObject.Find(hit.transform.name);
+                Debug.Log(hit.transform.name + " destroyed");
+
+                // Update Score
+                /* Bee: In formation    50
+                    Bee: Diving 100
+                    Butterfly: In formation 80
+                    Butterfly: Diving   160 
+                    Boss Galaga: In formation   150
+                    Boss Galaga: Diving alone   400                      
+                    */
+                if (obj.name.Contains("Bee")) //Bee
                 {
-                    // Get gameobj from ship hit
-                    GameObject obj = GameObject.Find(hit.transform.name);
-                    Debug.Log(hit.transform.name + " destroyed");
-
-                    // Update Score
-                    /* Bee: In formation    50
-                        Bee: Diving 100
-                        Butterfly: In formation 80
-                        Butterfly: Diving   160 
-                        Boss Galaga: In formation   150
-                        Boss Galaga: Diving alone   400                      
-                        */
-                    if (obj.name.Contains("Green")) //Bee
+                    if (obj.tag == "Diving")
                     {
-                        if(obj.tag == "Diving")
-                        {
-                            score += 100;
-                        }
-                        else
-                        {
-                            score += 50;
-                        }
+                        score += 100;
                     }
-                    else if (obj.name.Contains("Yellow")) //Butterfly
+                    else
                     {
-                        if (obj.tag == "Diving")
-                        {
-                            score += 160;
-                        }
-                        else
-                        {
-                            score += 80;
-                        }
+                        score += 50;
                     }
-                    else if (obj.name.Contains("Orange")) //Boss Galaga
-                    {
-                        if (obj.tag == "Diving")
-                        {
-                            score += 400;
-                        }
-                        else
-                        {
-                            score += 150;
-                        }
-                    }
-                    Debug.Log("Score: " + score);
-
-                    // Destroy game object
-                    GameObject.Destroy(obj);
                 }
+                else if (obj.name.Contains("Butterfly")) //Butterfly
+                {
+                    if (obj.tag == "Diving")
+                    {
+                        score += 160;
+                    }
+                    else
+                    {
+                        score += 80;
+                    }
+                }
+                else if (obj.name.Contains("Boss")) //Boss Galaga
+                {
+                    if (obj.tag == "Diving")
+                    {
+                        score += 400;
+                    }
+                    else
+                    {
+                        score += 150;
+                    }
+                }
+
+                //Update Score
+                Debug.Log("Score: " + score);
+                GameObject textObject1 = GameObject.Find("Score");
+                textObject1.GetComponent<TextMesh>().text = "Score: " + score;
+
+                // Destroy game object
+                GameObject.Destroy(obj);
             }
         }
     }
---
+++ Assets/Scenes/uiScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class uiScript : MonoBehaviour
+{
+    public static SortedList leaderboard = new SortedList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Start game
+    public static void startGame(bool newGame)
+    {
+        Debug.Log("Game Started");
+        //Do countdown
+
+        //Spawn first wave of ships
+        if (newGame)
+        {
+
+        }
+        else
+        {
+            //Start game with old configuration
+        }
+
+        // Set game as active
+        galagaMoveShip.gameActive = true;
+    }
+
+    // Pause game
+    public static void pauseGame(bool liveDecrement)
+    {
+        Debug.Log("Game Paused");
+        // Set game as inactive
+        galagaMoveShip.gameActive = false;
+
+        //Player lost life
+        if (liveDecrement)
+        {
+            // Update Lives
+            galagaMoveShip.lives--;
+            GameObject textObject1 = GameObject.Find("Lives");
+            textObject1.GetComponent<TextMesh>().text = "Lives: " + galagaMoveShip.lives;
+
+            // Restart Game or stop
+            if(galagaMoveShip.lives > 0)
+            {
+                startGame(false);
+            }
+            else
+            {
+                stopGame();
+            }
+
+        }
+
+        // Load UI to make further selections
+    }
+
+    // Quit game
+    public static void stopGame()
+    {
+        Debug.Log("Game Stopped");
+        // Set game as inactive
+        galagaMoveShip.gameActive = false;
+
+        // Write high score to leaderboard and save
+        int finalScore = galagaMoveShip.score;
+        leaderboard.Add(finalScore, System.DateTime.Now.ToString("MM/dd/yyyy/"));
+        save();
+
+        // Reset globals
+        galagaMoveShip.score = 0;
+        galagaMoveShip.lives = 3;
+        galagaMoveShip.gameSpeed = 1.0;
+
+        // Change scenes
+    }
+
+    // Load leaderboard
+    static void load()
+    {
+        // Load Galaga Leaderboard
+        if (File.Exists(Application.persistentDataPath + "/galaga.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.OpenRead(Application.persistentDataPath + "/galaga.dat");
+            SortedList newLeaderboard = (SortedList)bf.Deserialize(fs);
+            fs.Close();
+
+            leaderboard = newLeaderboard;
+
+
+        }
+    }
+
+    // Save leaderboard
+    static void save()
+    {
+        // Save Galaga Leaderboard
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs1 = File.Open(Application.persistentDataPath + "/galaga.dat", FileMode.OpenOrCreate);
+        bf.Serialize(fs1, leaderboard);
+        fs1.Close();
+    }
+}
